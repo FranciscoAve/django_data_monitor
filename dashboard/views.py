b6d072846a574e2f0e94b3dab860b73d2374a3bc
Francisco Avendaño <favendan@espol.edu.ec>
2026-01-14 06:07:40 +0000

    guia24 checkkkk omg

--- dashboard/views.py
+++ dashboard/views.py
@@ -1,7 +1,19 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+import requests
+from django.conf import settings
 
 # Create your views here.
 def index(request):
+
+    response = requests.get(settings.API_URL)
+    posts = response.json()
+
+    total_responses = len(posts)
+
+    data = {
+        "title": "Landing Page' Dashboard",
+        "total_responses": total_responses
+    }
     #return HttpResponse("¡Bienvenido a la aplicación Django!")
-    return render(request, 'dashboard/base.html')
+    return render(request, 'dashboard/index.html', data)
